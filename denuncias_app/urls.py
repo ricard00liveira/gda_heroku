859cd7645cafcb2710dfd15dd8c1e0984e5da513
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2024-11-05 22:36:39 -0300

    primeiros passos no back

---
+++ denuncias_app/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('denuncias/', lista_denuncias, name='lista_denuncias'),
+    path('ver_denuncias/', view_lista_denuncias, name='view_lista_denuncias'),
+]
--- denuncias_app/views.py
+++ denuncias_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Denuncia
+from .serializers import DenunciaSerializer
 
-# Create your views here.
+class DenunciaViewSet(viewsets.ModelViewSet):
+    queryset = Denuncia.objects.all()
+    serializer_class = DenunciaSerializer
+
+@api_view(['GET'])
+def lista_denuncias(request):
+    denuncias = Denuncia.objects.all()
+    serializer = DenunciaSerializer(denuncias, many=True)
+    return Response(serializer.data)
+
+def view_lista_denuncias(request):
+    return render(request, 'denuncias_list.html')
