c335c13c11ed98ff30c701d4eae9eea26c0c5960
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2024-11-05 23:36:35 -0300

    fix bugs deploy

--- gda/settings.py
+++ gda/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import django_heroku
+import os
 
 django_heroku.settings(locals())
 
@@ -143,7 +144,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
