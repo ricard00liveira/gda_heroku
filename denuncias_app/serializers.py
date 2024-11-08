f413346cd05ceedf9068e2325fc7f6b0a4485f47
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2024-11-08 00:04:50 -0300

    ok para deploy

--- denuncias_app/serializers.py
+++ denuncias_app/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Denuncia
+from .models import Denuncia, Municipio
 
 class DenunciaSerializer(serializers.ModelSerializer):
     class Meta:
         model = Denuncia
+        fields = '__all__'
+
+class MunicipioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Municipio
         fields = '__all__'
--- gda/settings.py
+++ gda/settings.py
@@ -58,6 +58,7 @@ MIDDLEWARE = [
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
+    'https://gda-front.netlify.app'
     # URL do frontend React
 ]
 
