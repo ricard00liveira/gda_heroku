fbda375b3cea742072aedfe4c3112b0b0fb563a4
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2025-05-20 17:25:08 -0300

    teste deploy heroku

--- gda/settings.py
+++ gda/settings.py
@@ -208,3 +208,4 @@ GDAL_LIBRARY_PATH = os.environ.get("GDAL_LIBRARY_PATH")
 GEOS_LIBRARY_PATH = os.environ.get("GEOS_LIBRARY_PATH")
 
 django_heroku.settings(locals())
+DATABASES["default"]["ENGINE"] = "django.contrib.gis.db.backends.postgis"
