b25350695d1e0e4dcb6ed2e71d96f51ea9293fd5
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2025-05-20 17:10:38 -0300

    rollback settings.py

--- gda/urls.py
+++ gda/urls.py
@@ -9,7 +9,7 @@ from drf_yasg import openapi
 schema_view = get_schema_view(
     openapi.Info(
         title="API GDA",
-        default_version='v1',
+        default_version="v1",
         description="Documentação da API do sistema GDA",
     ),
     public=True,
@@ -17,18 +17,20 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls), # ADMIN
-    path('api/', include('denuncias.urls')),  # Rota Denuncias
-    path('api/', include('enderecos.urls')), # Rota Enderecos
-    path('api/', include('usuarios.urls')), # Rota Usuarios
-    path('api/', include('fatosesub.urls')), # Rota Fatos e Subfatos
-    
+    path("admin/", admin.site.urls),  # ADMIN
+    path("api/", include("denuncias.urls")),  # Rota Denuncias
+    path("api/", include("enderecos.urls")),  # Rota Enderecos
+    path("api/", include("usuarios.urls")),  # Rota Usuarios
+    path("api/", include("fatosesub.urls")),  # Rota Fatos e Subfatos
     # TOKEN
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     # SWAGGER
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-    path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    path("swagger.json", schema_view.without_ui(cache_timeout=0), name="schema-json"),
 ]
