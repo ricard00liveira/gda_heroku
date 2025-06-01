87aea79a86e5c58c508cc975e718bfb3b1f76ec9
Ricardo P Oliveira <ricardooliveira.pl216@academico.ifsul.edu.br>
2025-06-01 18:42:55 -0300

    Libera acesso público à interface Swagger

--- gda/urls.py
+++ gda/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from rest_framework import permissions
+from rest_framework.permissions import AllowAny
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
@@ -13,6 +14,7 @@ schema_view = get_schema_view(
         description="Description",
     ),
     public=True,
+    permission_classes=[AllowAny],
 )
 
 urlpatterns = [
